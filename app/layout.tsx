787f5b81c596de4b1715b95c8217fd9498360128
Georgii Koloidi <battleaxe@bk.ru>
2025-05-04 18:56:22 +0100

    feat: add page routes

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./layout.scss";
+
+import Header from "@/modules/Header/Header";
+import Footer from "@/modules/Footer/Footer";
+import BootstrapClient from "@/shared/BootstrapClient/BootstrapClient";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Weather and Forecast",
+  description: "Test work",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+      <BootstrapClient />
+    </html>
+  );
+}
