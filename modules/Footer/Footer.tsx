5ff81f34225f507ff17b5d5f73c04d9341db88b7
Georgii Koloidi <battleaxe@bk.ru>
2025-05-05 08:30:41 +0100

    feat: add Footer

---
+++ modules/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-light py-4 mt-5">
+      <div className="container">
+        <p className="text-center mb-0">© 2025 Synoptic+</p>
+      </div>
+    </footer>
+  );
+}
