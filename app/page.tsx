09ba46d4ffb35a622b18560c0c044148841f1fd4
Georgii Koloidi <battleaxe@bk.ru>
2025-05-06 12:05:53 +0100

    fix: styles

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ export default function MainPage() {
   return (
     <>
       <section className={styles.heroSection}>
-        <div className="position-absolute top-50 start-50 translate-middle text-center w-100 px-3">
-          <h1 className="text-white mb-0">Weather forecast by locations</h1>
-        </div>
+        <h1 className="text-white mb-0">Weather forecast by locations</h1>
       </section>
       <section className="container justify-content-center pt-5 d-flex flex-column gap-4">
         <h2 className="row justify-content-center">Search for a location</h2>
