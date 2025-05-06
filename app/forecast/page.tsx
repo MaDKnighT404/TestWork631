42e6a4237a1c18917ee0618e40a3053898780ca5
Georgii Koloidi <battleaxe@bk.ru>
2025-05-06 10:53:43 +0100

    feat: add WeatherForecast to the ForecastPage

--- app/forecast/page.tsx
+++ app/forecast/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import WeatherForecast from "@/modules/WeatherForecast/WeatherForecast";
+
 export default function ForecastPage() {
-  return <div>ForecastPage</div>;
+  return (
+    <section className="container justify-content-center pt-5 d-flex flex-column gap-4">
+      <div className="col-12 md-10 mt-4 mx-auto d-flex flex-column gap-4">
+        <WeatherForecast />
+      </div>
+    </section>
+  );
 }
