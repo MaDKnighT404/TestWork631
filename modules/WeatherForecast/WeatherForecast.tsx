21a39f3d17e14a1c2d3e8f38baa0fa6ed687da47
Georgii Koloidi <battleaxe@bk.ru>
2025-05-06 10:14:20 +0100

    feat: add button to WeatherForecast module to add or remove fav cities

--- modules/WeatherForecast/WeatherForecast.tsx
+++ modules/WeatherForecast/WeatherForecast.tsx
@@ -2,29 +2,62 @@
 
 import React from "react";
 import Image from "next/image";
+import { redirect, usePathname } from "next/navigation";
 
-import { useWeatherForecastStore } from "./Store";
-
+import { useWeatherForecastStore } from "./store";
+import { useFavoriteCitiesStore } from "../FavoriteCities/store";
 import { selectWeatherForecastPeriod } from "./helpers/selectWeatherForecastPeriod";
 
 export default function WeatherForecast({ singleDay = false }: { singleDay?: boolean }) {
+  const pathname = usePathname();
   const { weatherForecastData } = useWeatherForecastStore();
+  const { favoriteCities, addFavoriteCity, removeFavoriteCity } = useFavoriteCitiesStore();
+
+  if (!weatherForecastData && pathname === "/forecast") {
+    redirect("/");
+  }
   if (!weatherForecastData) return null;
 
   const filteredWeatherForecast = selectWeatherForecastPeriod(weatherForecastData, singleDay);
   const title = singleDay
-    ? "Weather forecast by locations for next 9 hours"
+    ? "Weather forecast by locations for next 12 hours"
     : "Weather forecast by locations for 5 days";
+
+  const isFavoriteCity = favoriteCities.some((city) => city.name === weatherForecastData.city.name);
+
+  const handleChangeFavoriteCity = () => {
+    if (isFavoriteCity) {
+      removeFavoriteCity({
+        name: weatherForecastData.city.name,
+        lat: weatherForecastData.city.coord.lat,
+        lon: weatherForecastData.city.coord.lon,
+      });
+    } else {
+      addFavoriteCity({
+        name: weatherForecastData.city.name,
+        lat: weatherForecastData.city.coord.lat,
+        lon: weatherForecastData.city.coord.lon,
+      });
+    }
+  };
 
   return (
     <div className="mt-4">
       <h2 className="text-center">{title}</h2>
-      <h3 className="mt-5">
-        {weatherForecastData.city.name}, {weatherForecastData.city.country}
-      </h3>
-      <div className="row mt-3">
+      <div className="d-flex mt-5 align-items-center gap-3">
+        <h3 className="mb-0">
+          {weatherForecastData.city.name}, {weatherForecastData.city.country}
+        </h3>
+        <button
+          className={`btn btn-${isFavoriteCity ? "danger " : "primary"} `}
+          onClick={handleChangeFavoriteCity}
+        >
+          {isFavoriteCity ? "Remove from favorites" : "Add to favorites"}
+        </button>
+      </div>
+      <ul className="row mt-4 d-flex justify-content-center">
         {filteredWeatherForecast.map((item, index) => (
-          <div key={index} className={`col-${singleDay ? "12 col-md-3 mb-3" : "12 col-md-2 mb-3"}`}>
+          <li key={index} className="col-12 col-sm-5 col-lg mb-3">
             <div className="card h-100">
               <div className="card-body text-center">
                 <h5 className="card-title">
@@ -65,9 +98,9 @@ export default function WeatherForecast({ singleDay = false }: { singleDay?: boo
                 </div>
               </div>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
